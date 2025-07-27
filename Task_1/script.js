8b6a1f73239a033c1893599ca6b885d140f32543
Swati Singh <swatisingh.cg@gmail.com>
2025-07-28 01:00:35 +0530

    New update

--- Task_1/script.js
+++ Task_1/script.js
@@ -1,1 +1,11 @@
-S
+let counter = 0;
+let isProcessing = false;
+
+async function processItem(item) {
+  if (isProcessing) return;
+  isProcessing = true;
+  
+  await someAsyncOperation(item);
+  counter++;
+  isProcessing = false;
+}
